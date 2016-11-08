d2128e4ddf44a89b4b080107efe71bf47c25ae4d
Mateus Ildefonso <matecompufrj@gmail.com>
2016-11-08 21:02:47 -0200

    * update TDIDF

---
+++ TESI/NER/extractNE.py
@@ -0,0 +1,257 @@
+# -*- coding: utf-8 -*-
+import nltk
+import re
+import sys
+import csv as csv
+reload(sys)
+sys.setdefaultencoding('utf-8')
+
+#Para este trabalho utilizei o perceptronTagger...
+# import inspect
+# def pos_tag(tokens, tagset=None):
+# 	tagger = PerceptronTagger()
+# 	return _pos_tag(tokens, tagset, tagger)
+# from nltk import pos_tag
+
+
+#Cria uma lista com o endereco de todos os arquivos txt dentro de alguma pasta
+from os import listdir
+from os import walk
+from os.path import isfile, join
+def list_files(directory):
+	files = []
+	for (dirpath, dirnames, filenames) in walk(directory):
+		for dirname in dirnames:
+			path = directory+dirname
+			files.append([path+'/'+file for file in listdir(path) if isfile(join(path, file))])
+	return files
+
+#Metodos para extrair dado do arquivo JSON do Guilherme
+import json
+def openJson(archive):
+	with open(archive) as data_file:
+		data = json.load(data_file)
+	return data
+
+def Json2Content(data_json):
+	paragraph = data_json['summary']
+	text = data_json['info'] + '\n'
+	text += data_json['plot'] + '\n'
+	for p in paragraph:
+		text += p['content'] + '\n'
+	text = preprocessing(text)
+	return text
+
+def preprocessing(text):
+	text = text.decode("utf8")
+	#Remove as multiplas quebras de linha para extrair os dados iniciais
+	text = re.sub(r"\n\n+", ".\n", text)
+	#Remove caracteres indesejados
+	text = re.sub(u'[^a-zA-Z0-9.,\n \']', ' ', text)
+	#Separar , da palavra
+	text = re.sub(",", " , ", text)
+	#Remove espacos consecutivos
+	text = re.sub(r"  +", " ", text)
+	#Adiciona . em cada quebra de linha para ajudar a extracao de sentencas
+	text = re.sub(r"\.* *\n+", ".\n", text)
+	return text
+
+
+#Classifica gramaticalmente cada palavra do texto com uma tag
+def TaggerText(text):
+	#Extrair sentencas
+	tokenized_sentences = nltk.sent_tokenize(text)
+	#Extrair palavras
+	tokenized_words = [nltk.word_tokenize(sent) for sent in tokenized_sentences]
+	#Tagger lista de lista de words
+	tagged_sentences = [nltk.pos_tag(wordlist) for wordlist in tokenized_words]
+	#Renomear tags
+	temp = []
+	places = ["into","between","in","from"]
+	humans = ['king','lord commander','commander','lord','maester','lady','son','bastard'
+				'master','prince','princess','queen','sir','ser','regent','old']
+	for sent in tagged_sentences:
+		aux = []
+		for word,tag in sent:
+			aux_word = word.lower()
+			if aux_word == "of":
+				tag = "OF"
+			elif aux_word == "the":
+				tag = "DA"
+			elif word == "House":
+				tag = "HOUSE"
+			elif aux_word in places:
+				tag = "LOC"
+			elif aux_word in humans:
+				tag = "STATUS"
+			aux.append((word,tag))
+		temp.append(aux)
+
+	tagged_sentences = temp
+	return tagged_sentences
+
+#Remove os caracteres especiais adicionado no objeto subtree do NLTK
+def Subtree2Text(subtree):
+	text = str(subtree)
+	text = re.sub(r"\([A-Z]+ *| *[A-Z]+\)|/[A-Z]*\)?|\)","", text)
+	text = re.sub(r" *\n+"," ", text)
+	text = re.sub(r"  +"," ", text)
+	text = re.sub(r"^\s+","", text)
+	text = re.sub(u"[^a-zA-Z0-9 \']", '', text)
+	return text
+
+#Remove as entidades que estao contidas em outra entidade
+def removeSubstring(string_list):
+	words = set()
+	for s in string_list:
+		if not any([s+' ' in word or ' '+s in word or (' ' in s and s in word and s != word) or s in words for word in string_list]):
+			words.add(s)
+	return words
+
+
+def Chunker(tagged_sentences):
+
+	grammar = r"""
+	  NP:
+	    {<DT|PRP\$>?<JJ>*<NNP|NNPS>+}   # chunk determiner/possessive, adjectives and noun
+	    }<VBD|IN>+{      # Chink sequences of VBD and IN
+	  ORGANIZATION:
+	  	{<NP>+<OF><NP>+}
+	  HOUSES:
+	  	{<HOUSE><OF>?<NP>+}		# NP is better than anyway tag in this case
+
+	  """
+	# PEOPLE:
+	#   	{<STATUS><NP>+}
+	#   LOCATION:
+	#   	{<LOC><DA><NP\+|.*>}
+	cp = nltk.RegexpParser(grammar)
+	NE = set()
+	
+	for sent in tagged_sentences:
+		tree = cp.parse(sent)
+		for subtree in tree.subtrees():
+			if subtree.label() != "S":
+				entity = Subtree2Text(subtree)
+				lower_entity = entity.lower() + ": "
+				class_entity = subtree.label() + ": "
+				text = entity #class_entity + lower_entity + entity
+				if text not in NE:
+					NE.add(text)
+	return NE
+
+def extractNE(data_json):
+	episode_text = Json2Content(data_json)
+	tagged_sentences = TaggerText(episode_text)
+	NE_aux = Chunker(tagged_sentences)
+	NE = set()
+	#Remove entidades que apareceram uma unica vez
+	for n in NE_aux:
+		freq_n = episode_text.count(n)
+		if freq_n > 1 or "house " in n.lower() or "game " in n.lower():
+			NE.add(n)
+
+	episode_text = episode_text.lower()
+
+	return NE, episode_text
+
+def allNER():
+	NE = set()
+	episode_text = ""
+	for season in seasons:
+		for episode in season:
+			print "Processing: ", episode
+			data_json = openJson(episode)
+			tempNE, tempText = extractNE(data_json)
+			NE.update(tempNE)
+			episode_text += tempText + '\n'
+	return NE, episode_text
+
+def markNER(text):
+	callback = lambda pat: pat.group(0).upper()
+	uNE = sorted(NE, reverse=True)
+	stopwords = nltk.corpus.stopwords.words("english")
+	stopwords.append(["first","news","three"])
+	for n in uNE:
+		n = n.lower()
+		n = n.split(" ")
+		for i in n:
+			if len(i) > 2 and i not in stopwords:
+				log_str = "( +|\.|\n)"+i+"( +|\.|\n|s|'s|')"
+				text = re.sub(log_str, callback, text, flags=re.IGNORECASE)
+
+	callback = lambda pat: pat.group(0).lower()
+	text = re.sub(r"([A-Z])+([a-z])", callback, text)
+	text = re.sub(r"([a-z])([A-Z])+", callback, text)
+	text = re.sub(r"'S", " 'S", text)
+	text = re.sub(r"'", " '", text)
+	return text
+
+def pre_NCE_Classifier(marked_text):
+	valid_text = ""
+	for line in marked_text.split('\n'):
+		if len(line) > 60:
+			valid_text += "BEGIN BEGIN " + line + " END END\n"
+
+	# valid_text = re.sub(r"'s", "", valid_text)
+	valid_text = re.sub(r"\. ", " END END BEGIN BEGIN ", valid_text)
+	valid_text = re.sub(r"BEGIN BEGIN END END", "", valid_text)
+	valid_text = re.sub(u'[^a-zA-Z0-9.\n \']', '', valid_text)
+	return valid_text
+
+
+def saveCSV(name, values):
+	#Etapas para criar o arquivo csv e escrever todo conteudo em N neste arquivo
+	target_file = open(name, "wb")
+	open_file_object = csv.writer(target_file)
+	open_file_object.writerow(["NER"])
+	for item in values:
+		open_file_object.writerow([item,])
+	target_file.close()
+
+def loadCSV(archive):
+	NE = set()
+	with open(archive, 'rb') as csvfile:
+		words = csv.reader(csvfile)
+		for word in words:
+			if word[0] != "NER":
+				NE.update(word)
+	return NE
+
+
+if __name__ == '__main__':
+	#Pasta de onde os textos serao obtidos
+	mypath = "../episodesJSON"
+	#Lista com o endereco de cada arquivo txt
+	seasons = list_files(mypath)
+	#Lista ordenada sem repeticao das endidades
+	# NE, episode_text = allNER()
+	NE = loadCSV("Naive-NER.csv")
+
+	NE = removeSubstring(NE)
+	NE = sorted(NE)
+
+	#Extrai o corpus de GoT
+	f = open("../DATASET/episode_text.txt", "rb")
+	episode_text = f.read()
+	f.close()
+
+	#Marca as entidades no corpus de GoT
+	marked_text = markNER(episode_text)
+
+	#Preprocessamento para o tensorflow
+	valid_text = pre_NCE_Classifier(marked_text)
+
+	f = open("../DATASET/base_de_dados.txt", "wb")
+	f.write(marked_text)
+	f.close()
+
+	f = open("../DATASET/base_de_dados_nce.txt", "wb")
+	f.write(valid_text)
+	f.close()
+
+	print len(NE)
+	saveCSV('Naive-NER.csv', NE)
+	print "Done"
+			
+			
---
+++ TESI/wordEMBEDDINGS/Word2Vec.py
@@ -0,0 +1,332 @@
+# Copyright 2015 The TensorFlow Authors. All Rights Reserved.
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+# ==============================================================================
+
+from __future__ import absolute_import
+from __future__ import division
+from __future__ import print_function
+
+import collections
+import math
+import os
+import random
+import zipfile
+
+import numpy as np
+from six.moves import urllib
+from six.moves import xrange  # pylint: disable=redefined-builtin
+import tensorflow as tf
+
+# Read the data into a list of strings.
+def read_data(filename):
+  """Extract the first file enclosed in a zip file as a list of words"""
+  with zipfile.ZipFile(filename) as f:
+    data = tf.compat.as_str(f.read(f.namelist()[0])).split()
+  return data
+
+#Build the dictionary and replace rare words with UNK token.
+# vocabulary_size = 50000
+
+def build_dataset(words, vocabulary_size):
+  count = [['UNK', -1]]
+  count.extend(collections.Counter(words).most_common(vocabulary_size - 1))
+  dictionary = dict()
+  for word, _ in count:
+    dictionary[word] = len(dictionary)
+  data = list()
+  unk_count = 0
+  for word in words:
+    if word in dictionary:
+      index = dictionary[word]
+    else:
+      index = 0  # dictionary['UNK']
+      unk_count += 1
+    data.append(index)
+  count[0][1] = unk_count
+  reverse_dictionary = dict(zip(dictionary.values(), dictionary.keys()))
+  return data, count, dictionary, reverse_dictionary
+
+
+data_index = 0
+# Step 3: Function to generate a training batch for the skip-gram model.
+def generate_batch(batch_size, num_skips, skip_window, data):
+  global data_index
+  assert batch_size % num_skips == 0
+  assert num_skips <= 2 * skip_window
+  batch = np.ndarray(shape=(batch_size), dtype=np.int32)
+  labels = np.ndarray(shape=(batch_size, 1), dtype=np.int32)
+  span = 2 * skip_window + 1 # [ skip_window target skip_window ]
+  buffer = collections.deque(maxlen=span)
+  for _ in range(span):
+    buffer.append(data[data_index])
+    data_index = (data_index + 1) % len(data)
+  for i in range(batch_size // num_skips):
+    target = skip_window  # target label at the center of the buffer
+    targets_to_avoid = [ skip_window ]
+    for j in range(num_skips):
+      while target in targets_to_avoid:
+        target = random.randint(0, span - 1)
+      targets_to_avoid.append(target)
+      batch[i * num_skips + j] = buffer[skip_window]
+      labels[i * num_skips + j, 0] = buffer[target]
+    buffer.append(data[data_index])
+    data_index = (data_index + 1) % len(data)
+  return batch, labels
+
+import matplotlib.pyplot as plt
+from sklearn.cluster import KMeans
+
+def plot_with_labels(low_dim_embs, labels, filename='tsne.png', y_pred=None):
+  assert low_dim_embs.shape[0] >= len(labels), "More labels than embeddings"
+  plt.figure(figsize=(24, 24))  #in inches
+  colors = ["blue","green","red","yellow","black","purple"]
+  flag = 1
+  if y_pred is None:
+  	flag = 0
+  for i, label in enumerate(labels):
+    x, y = low_dim_embs[i,:]
+    if flag == 0:
+    	plt.scatter(x, y, color="green")
+    else:
+		plt.scatter(x, y, color=colors[y_pred[i]])
+
+    plt.annotate(label,
+                 xy=(x, y),
+                 xytext=(5, 2),
+                 textcoords='offset points',
+                 ha='right',
+                 va='bottom')
+
+  plt.savefig(filename)
+
+final_embeddings = []
+reverse_dictionary = dict()
+dictionary = dict()
+data = []
+count = []
+def word2vec(filename, vocabulary_size, num_steps):
+	global final_embeddings
+	global reverse_dictionary
+	global dictionary
+	global data
+	global count
+	words = read_data(filename)
+	print('Data size', len(words))
+	data, count, dictionary, reverse_dictionary = build_dataset(words, vocabulary_size)
+	print(len(dictionary))
+	del words  # Hint to reduce memory.
+	print('Most common words (+UNK)', count[:5])
+	print('Sample data', data[:10], [reverse_dictionary[i] for i in data[:10]])
+
+	batch, labels = generate_batch(batch_size=8, num_skips=2, skip_window=1, data=data)
+
+	for i in range(8):
+  		print(batch[i], reverse_dictionary[batch[i]],
+      		'->', labels[i, 0], reverse_dictionary[labels[i, 0]])
+
+	# Step 4: Build and train a skip-gram model.
+
+	batch_size = 256
+	embedding_size = 256  # Dimension of the embedding vector.
+	skip_window = 2       # How many words to consider left and right.
+	num_skips = 4         # How many times to reuse an input to generate a label.
+
+	# We pick a random validation set to sample nearest neighbors. Here we limit the
+	# validation samples to the words that have a low numeric ID, which by
+	# construction are also the most frequent.
+	valid_size = 16     # Random set of words to evaluate similarity on.
+	valid_window = 100  # Only pick dev samples in the head of the distribution.
+	valid_examples = np.random.choice(valid_window, valid_size, replace=False)
+	num_sampled = 64    # Number of negative examples to sample.
+
+	graph = tf.Graph()
+
+	with graph.as_default():
+
+	  # Input data.
+	  train_inputs = tf.placeholder(tf.int32, shape=[batch_size])
+	  train_labels = tf.placeholder(tf.int32, shape=[batch_size, 1])
+	  valid_dataset = tf.constant(valid_examples, dtype=tf.int32)
+
+	  # Ops and variables pinned to the CPU because of missing GPU implementation
+	  with tf.device('/cpu:0'):
+	    # Look up embeddings for inputs.
+	    embeddings = tf.Variable(
+	        tf.random_uniform([vocabulary_size, embedding_size], -1.0, 1.0))
+	    embed = tf.nn.embedding_lookup(embeddings, train_inputs)
+
+	    # Construct the variables for the NCE loss
+	    nce_weights = tf.Variable(
+	        tf.truncated_normal([vocabulary_size, embedding_size],
+	                            stddev=1.0 / math.sqrt(embedding_size)))
+	    nce_biases = tf.Variable(tf.zeros([vocabulary_size]))
+
+	  # Compute the average NCE loss for the batch.
+	  # tf.nce_loss automatically draws a new sample of the negative labels each
+	  # time we evaluate the loss.
+	  loss = tf.reduce_mean(
+	      tf.nn.sampled_softmax_loss(nce_weights, nce_biases, embed, train_labels,
+	                     num_sampled, vocabulary_size))
+
+	  # Construct the SGD optimizer using a learning rate of 1.0.
+	  optimizer = tf.train.GradientDescentOptimizer(1.0).minimize(loss)
+
+	  # Compute the cosine similarity between minibatch examples and all embeddings.
+	  norm = tf.sqrt(tf.reduce_sum(tf.square(embeddings), 1, keep_dims=True))
+	  normalized_embeddings = embeddings / norm
+	  valid_embeddings = tf.nn.embedding_lookup(
+	      normalized_embeddings, valid_dataset)
+	  similarity = tf.matmul(
+	      valid_embeddings, normalized_embeddings, transpose_b=True)
+
+	  # Add variable initializer.
+	  init = tf.initialize_all_variables()
+
+	# Begin training.
+	# num_steps = 100001
+
+	with tf.Session(graph=graph) as session:
+	  # We must initialize all variables before we use them.
+	  init.run()
+	  print("Initialized")
+
+	  average_loss = 0
+	  for step in xrange(num_steps):
+	    batch_inputs, batch_labels = generate_batch(
+	        batch_size, num_skips, skip_window, data)
+	    feed_dict = {train_inputs : batch_inputs, train_labels : batch_labels}
+
+	    # We perform one update step by evaluating the optimizer op (including it
+	    # in the list of returned values for session.run()
+	    _, loss_val = session.run([optimizer, loss], feed_dict=feed_dict)
+	    average_loss += loss_val
+
+	    if step % 2000 == 0:
+	      if step > 0:
+	        average_loss /= 2000
+	      # The average loss is an estimate of the loss over the last 2000 batches.
+	      print("Average loss at step ", step, ": ", average_loss)
+	      average_loss = 0
+
+	    # Note that this is expensive (~20% slowdown if computed every 500 steps)
+	    if step % 10000 == 0:
+	      sim = similarity.eval()
+	      for i in xrange(valid_size):
+	        valid_word = reverse_dictionary[valid_examples[i]]
+	        top_k = 8 # number of nearest neighbors
+	        nearest = (-sim[i, :]).argsort()[1:top_k+1]
+	        log_str = "Nearest to %s:" % valid_word
+	        for k in xrange(top_k):
+	          close_word = reverse_dictionary[nearest[k]]
+	          log_str = "%s %s," % (log_str, close_word)
+	        print(log_str)
+	  final_embeddings = normalized_embeddings.eval()
+	  print(final_embeddings.shape)
+
+	save("wordspace/words_vector.json", "final_embeddings", final_embeddings)
+	y_pred = saveTSNE()
+	save("wordspace/words_labels.json", "y_pred", y_pred)
+
+def saveTSNE():
+	try:
+	  from sklearn.manifold import TSNE
+
+	  tsne = TSNE(perplexity=30, n_components=2, init='pca', n_iter=5000)
+	  plot_only = 500
+	  low_dim_embs = tsne.fit_transform(final_embeddings[:plot_only,:])
+	  y_pred = KMeans(n_clusters=6, random_state=170).fit_predict(low_dim_embs)
+	  labels = [reverse_dictionary[i] for i in xrange(plot_only)]
+	  if y_pred is None:
+	  	plot_with_labels(low_dim_embs, labels, filename="tsne.png")
+	  else:
+	  	plot_with_labels(low_dim_embs, labels, filename='tsne.png', y_pred=y_pred)
+
+	except ImportError:
+	  print("Please install sklearn, matplotlib, and scipy to visualize embeddings.")
+	return y_pred
+
+import json
+def save(filename,name,archive):
+    """Salva os valores do vies e dos pesos da rede num arquivo."""
+    filename = filename
+    data = {name: [v.tolist() for v in archive]}
+    f = open(filename, "w")
+    json.dump(data, f)
+    f.close()
+    
+def load(filename,name,numpy=False):
+    """Carrega os dados de alguma rede anteriormente treinada."""
+    f = open(filename, "r")
+    data = json.load(f)
+    f.close()
+    if numpy:
+    	data_array = [np.array(w) for w in data[name]]
+    	data_array = np.asarray(data_array)
+    else:
+    	data_array = [w for w in data[name]]
+    return data_array
+
+if __name__ == '__main__':
+
+	vocabulary_size = 8500
+	word2vec("novo_dado.zip",vocabulary_size, 50001)
+	final_embeddings = load("wordspace/words_vector.json", "final_embeddings", numpy=True)
+	y_pred = load("wordspace/words_labels.json", "y_pred", numpy=True)
+	words = read_data("novo_dado.zip")
+	data, count, dictionary, reverse_dictionary = build_dataset(words, vocabulary_size)
+	del words
+
+	words = [dictionary["CERSEI"],dictionary["ARYA"],
+			dictionary["JAIME"],dictionary["TYRION"], 
+			dictionary["THEON"], dictionary["JON"],
+			dictionary["SANSA"],dictionary["TYWIN"],
+			dictionary["BRIENNE"],dictionary["HODOR"],
+			dictionary["JOFFREY"],dictionary["RAMSAY"],
+			dictionary["ROBERT"],dictionary["DAENERYS"],
+			dictionary["EDDARD"],dictionary["OBERYN"],
+			dictionary["BRAN"],dictionary["JORAH"],
+			dictionary["MYRCELLA"],dictionary["MELISANDRE"],
+			dictionary["BRONN"],dictionary["MARGAERY"]]
+	
+	# target = final_embeddings[words]
+	# dist = final_embeddings.dot(target.T)
+	# # nearest = dist.argsort()[-10:]
+	# # final_embeddings[word]
+	# print(dist.shape)
+	# print(dist[nearest])
+	for i in words:
+		valid_word = reverse_dictionary[i]
+		target = final_embeddings[i]
+		he = final_embeddings[dictionary['he']] 
+		she = final_embeddings[dictionary['she']]
+		dist = final_embeddings.dot(target.T)
+		dist = dist*dist
+	  	nearest = dist[:].argsort()[-20:]
+	  	log_str = "Nearest to %s:" % valid_word
+	        for k in xrange(19):
+	          close_word = reverse_dictionary[nearest[-k-1]]
+	          log_str = "%s %s," % (log_str, close_word)
+	        print(log_str)
+		hedist = target.dot(he.T)
+		hedist *= hedist
+		shedist = target.dot(she.T)
+		shedist *= shedist
+		log_str = "%s Class: %s" % (valid_word,y_pred[i])
+		if hedist > shedist:
+			log_str += " macho"
+		else:
+			log_str += " femea"
+		print(log_str)
+		print("")
+	
